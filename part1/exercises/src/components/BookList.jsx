486d1c87f0453fb235124e4ef000d2c336dba832
Parvathy2021 <parvathyvenu14@gmail.com>
2024-08-19 07:32:50 -0500

    Part1 completed

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
    let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let book1 = "https://www.google.com/books/edition/Long_Island_Compromise/nqftEAAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+   let book2 = "https://www.google.com/books/edition/Funny_Story/m9PMEAAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+   let book3 = "https://www.google.com/books/edition/First_Lie_Wins/bDi4EAAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Long Island Compromise by Taffy Brodesser-Akner" />
+         <img src={book2} alt="Funny Story by Emily Henry" />
+         <img src={book3} alt="First Lie Wins: Reese's Book Club Pick (A Novel) by Ashley Elston" />
       </div>      
    );
 }
